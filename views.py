7a59558a5c5bef632666da53fb37420b9f8a35de
Fowaliori <n1toleclown@gmail.com>
2022-12-09 22:02:03 +0700

    main_Solution

---
+++ views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    data ={
+        'title': 'Список аниме',
+        'animes': ['Naruto','Bleach', 'Evangelion']
+    }
+    return render(request, 'main/index.html', data)
+
+
+def about(request):
+    data ={
+        'title': 'Авторы',
+        'authors': ['Масаси Кисимото', 'Тайто Кубо', 'Ёсиюки Садамото']
+    }
+    return render(request, 'main/about.html',data)
